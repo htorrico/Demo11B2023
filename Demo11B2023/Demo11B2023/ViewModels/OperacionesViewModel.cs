3da8aed3ac1d172ef1a322ef4ebe0985586bd3af
htorricotecsup <53871912+htorrico@users.noreply.github.com>
2023-06-09 15:19:38 -0500

    NotifyPropertyChanged

--- Demo11B2023/Demo11B2023/ViewModels/OperacionesViewModel.cs
+++ Demo11B2023/Demo11B2023/ViewModels/OperacionesViewModel.cs
@@ -6,11 +6,56 @@ using Xamarin.Forms;
 
 namespace Demo11B2023.ViewModels
 {
-    public class OperacionesViewModel
+    public class OperacionesViewModel : ViewModelBase
     {
-        public int ValorA { get; set; }
-        public int ValorB { get; set; }
-        public int Resultado { get; set;}
+        //public int ValorA { get; set; }
+
+        int valorA;
+        public int ValorA
+        {
+            get { return valorA; }
+            set
+            {
+                if (valorA != value)
+                {
+                    valorA = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+
+        int valorB;
+        public int ValorB
+        {
+            get { return valorB; }
+            set
+            {
+                if (valorB != value)
+                {
+                    valorB = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        int resultado;
+        public int Resultado
+        {
+            get { return resultado; }
+            set
+            {
+                if (resultado != value)
+                {
+                    resultado = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
 
         public ICommand Sumar { get; set; }
         public ICommand Restar { get; set; }
