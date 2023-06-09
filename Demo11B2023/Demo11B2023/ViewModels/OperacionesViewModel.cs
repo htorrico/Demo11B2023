2592b2255d434b2f39f4b6a0b1ede53aef0af0f6
htorricotecsup <53871912+htorrico@users.noreply.github.com>
2023-06-09 15:08:17 -0500

    Add project files.

---
+++ Demo11B2023/Demo11B2023/ViewModels/OperacionesViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Demo11B2023.ViewModels
+{
+    public class OperacionesViewModel
+    {
+        public int ValorA { get; set; }
+        public int ValorB { get; set; }
+        public int Resultado { get; set;}
+
+        public ICommand Sumar { get; set; }
+        public ICommand Restar { get; set; }
+
+        public OperacionesViewModel()
+        {
+            Sumar = new Command(() =>
+            {
+                Resultado = ValorA + ValorB;
+            });
+
+
+            Restar = new Command(() =>
+            {
+                Resultado = ValorA + ValorB;
+            });
+        }
+
+    }
+}
